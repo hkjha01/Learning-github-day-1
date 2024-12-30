0a1840db81f9e19da95c9a2671a0f3fd9d3a7f0e
hkjha01 <hkjha01@gmail.com>
2024-12-30 11:20:41 +0530

    Add files via upload

---
+++ lec4 .py
@@ -0,0 +1,9 @@
+#dictionary-they are mutable  stor data in form of key:("val1","val2")
+
+a={"name":"ab",
+   "id" : "12",
+   "per":("90.9","98.9","80.9")}
+
+print(type(a))
+print(a["name"])
+print(a)
